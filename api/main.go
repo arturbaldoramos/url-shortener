341313b0625035e4bc5c7ddfac0cd50bbe244c3b
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-05-10 04:16:31 -0300

    feat: main

--- api/main.go
+++ api/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Valgard/godotenv"
+	"github.com/arturbaldoramos/url-shortener/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
+	"os"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Get("/api/v1", routes.ShortenURL)
+}
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err)
+	}
+	app := fiber.New()
+	app.Use(logger.New())
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 
 }
