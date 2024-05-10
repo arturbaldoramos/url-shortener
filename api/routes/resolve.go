f27f4ed88d2f51ac87e01b9b30c339233d265b8c
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-05-10 16:38:06 -0300

    feat: resolver

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,32 @@
 package routes
+
+import (
+	"github.com/arturbaldoramos/url-shortener/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to database",
+		})
+	}
+
+	rInc := database.CreateClient(1)
+	defer rInc.Close()
+
+	_ = rInc.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
