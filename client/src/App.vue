78e0ad516f0ab19f74c5aaf133c37c4eb2d67f39
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-06-04 14:28:35 -0300

    chore: i hate style

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,7 +9,8 @@ const value = ref(null);
 
 <template>
   <header>
-    <div class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+    <div
+      class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
       <div class="max-w-7xl mx-auto">
         <div class="border-b border-slate-900/10 py-4 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
           <div class="relative flex items-center">
@@ -17,24 +18,27 @@ const value = ref(null);
               <span class="text-sky-600">URL</span>
               <span class="text-white">Shortener</span>
             </a>
-            <a class="font-medium hover:text-sky-400 ml-auto" href="/login">Sign in</a>
+            <a class="duration-200 font-medium hover:text-sky-400 ml-auto" href="/login">Sign in</a>
           </div>
         </div>
       </div>
     </div>
   </header>
 
-  <main class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-    <Card class="w-2/4 text-center bg-slate-300">
-      <template #title>Paste your url here</template>
-      <template #content>
-        <form class="flex flex-row space-x-4">
-          <InputText class="w-full" type="text" v-model="value" />
-          <Button class="" label="Short" />
-        </form>
-      </template>
-    </Card>
+  <main class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 py-32 ">
+    <form class="flex space-x-4 h-20 justify-center">
+      <InputText class="input" type="text" v-model="value" placeholder="Paste your url here" />
+      <Button class="button" label="Short" />
+    </form>
+
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.input {
+  @apply bg-slate-800 w-1/2 border-slate-700 hover:border-sky-400 !important
+}
+.button{
+  @apply bg-sky-600 w-24 hover:bg-sky-400 border-slate-700 !important
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
-import './assets/index.css'
 import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from 'primevue/themes/aura';
+import './assets/index.css'
 
 const app = createApp(App);
 app.use(PrimeVue, {
