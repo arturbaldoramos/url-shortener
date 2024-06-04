3da507a6fd24abd6a524c019309f3066ee95e62d
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-06-04 18:30:41 -0300

    chore: change input

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Card from 'primevue/card';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import InputGroup from 'primevue/inputgroup';
 import { ref } from 'vue';
 
 const value = ref(null);
@@ -14,7 +14,7 @@ const value = ref(null);
       <div class="max-w-7xl mx-auto">
         <div class="border-b border-slate-900/10 py-4 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
           <div class="relative flex items-center">
-            <a class="space-x-2 text-3xl font-bold">
+            <a href="/" class="space-x-2 text-3xl font-bold">
               <span class="text-sky-600">URL</span>
               <span class="text-white">Shortener</span>
             </a>
@@ -25,20 +25,55 @@ const value = ref(null);
     </div>
   </header>
 
-  <main class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 py-32 ">
+  <main class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 py-32 space-y-32">
     <form class="flex space-x-4 h-20 justify-center">
-      <InputText class="input" type="text" v-model="value" placeholder="Paste your url here" />
-      <Button class="button" label="Short" />
+      <InputGroup class="input-group">
+        <InputText class="input" type="text" v-model="value" placeholder="Paste your url here" />
+        <Button class="button" type="submit" label="Compress" />
+      </InputGroup>
     </form>
 
+    <div class="flex-row">
+      <div class="grid grid-cols-3 items-center gap-12">
+        <div class="text-center space-y-2">
+          <i class="pi pi-box text-sky-400" style="font-size: 2.2rem"></i>
+          <div>
+            <h3 class="text-xl font-normal text-slate-100">Simple</h3>
+            <p>Easy and quick, simply input your long link to instantly receive a shortened URL.</p>
+          </div>
+        </div>
+
+        <div class="text-center space-y-2">
+          <i class="pi pi-bolt text-sky-400 rotate-12" style="font-size: 2.2rem"></i>
+          <div>
+            <h3 class="text-xl font-normal text-slate-100">Blazingly Fast</h3>
+            <p>Effortlessly convert long URLs into compact, shareable links in an instant.</p>
+          </div>
+        </div>
+
+        <div class="text-center space-y-2">
+          <i class="pi pi-wallet text-sky-400" style="font-size: 2.2rem"></i>
+          <div>
+            <h3 class="text-xl font-normal text-slate-100">Costless</h3>
+            <p>We don't want your money, just your satisfaction in simplifying long URLs into concise, manageable links!
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-.input {
-  @apply bg-slate-800 w-1/2 border-slate-700 hover:border-sky-400 !important
+.input-group{
+  @apply w-2/3
 }
-.button{
-  @apply bg-sky-600 w-24 hover:bg-sky-400 border-slate-700 !important
+.input {
+  @apply bg-slate-800 border-slate-700 hover:border-sky-400 !important
+}
+
+.button {
+  @apply bg-sky-600 hover:bg-sky-400 border-slate-700 !important
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-import 'primeicons/primeicons.css'
+import 'primeicons/primeicons.css';    
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
